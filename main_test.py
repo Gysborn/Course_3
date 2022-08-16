bdec43c3f5f200ea992726f707a99526916fba1e
Roy <forparty@gmail.com>
2022-08-16 09:48:04 +0300

    Final realise with fix bags

--- main_test.py
+++ main_test.py
@@ -10,7 +10,7 @@ def test_app():
     assert len(response.json) == 8
     assert response.json[2]['pk'] == 3, "ошибкаыва"
 
-"""
+
 def test_app():
 
     response = app.test_client().get('/api/posts/1')
@@ -19,11 +19,3 @@ def test_app():
     # assert response.data == render_template('index.html')
     #assert len(response.json) == 1
     assert response.json['pk'] == 2, "ошибка"
-"""
-def test_app():
-    response = app.test_client().get('/post/3')
-    assert response.status_code == 200
-    #print(response.json)
-    # assert response.data == render_template('index.html')
-    assert len(response.json) == 8
-    assert response.json['pk'] == 3, "ошибкаыва"
