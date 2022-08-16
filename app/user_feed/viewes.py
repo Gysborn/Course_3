13005a5b407a513626336a39bd0c079ca83a3472
Roy <forparty@gmail.com>
2022-08-16 20:13:08 +0300

    Final realise with fix bags1.1

--- app/user_feed/viewes.py
+++ app/user_feed/viewes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, abort
 
-from app.dao.main_dao import get_posts_by_user
+from app.dao.main_dao import get_posts_by_user, get_id_bookmarks
 
 user_feed_blueprint = Blueprint('user_feed_blueprint', __name__)
 
@@ -10,4 +10,4 @@ def user_feed(name):
     user_posts = get_posts_by_user(name)
     if not user_posts:
         return abort(404)
-    return render_template('user-feed.html', user_posts=user_posts, name=name)
+    return render_template('user-feed.html', user_posts=user_posts, name=name, bookmarks=get_id_bookmarks())
